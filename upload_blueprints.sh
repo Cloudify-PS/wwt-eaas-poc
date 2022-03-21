1e72fed24ab2a5a4300811e6e447674ad3b78a66
Michał Mordawski <michal.mordawski@amartus.com>
2022-03-21 09:58:23 +0100

    GitHub Actions - support for any tenant

--- upload_blueprints.sh
+++ upload_blueprints.sh
@@ -9,6 +9,10 @@ cfy secrets create eaas_params -uf secrets/eaas_params_${1:-dev}.json
 zip -r aws_blueprint.zip aws/
 cfy blueprint upload -b aws-nginx -n aws-nginx.yaml aws_blueprint.zip
 
+# Upload Terraform blueprint
+zip -r aws_blueprint.zip aws/
+cfy blueprint upload -b terraform-repository https://github.com/Cloudify-PS/wwt-tf-templates/archive/refs/heads/master.zip
+
 # Upload GCP blueprint
 zip -r gcp_blueprint.zip gcp/
 cfy blueprint upload -b gcp-nginx -n gcp-nginx.yaml gcp_blueprint.zip
